c8d3b51b0340366a0756bf1aeef6ec794399c026
김태건 <121171046+taegun-kim@users.noreply.github.com>
2024-11-11 21:13:58 +0900

    Update rain.ino

--- rain.ino
+++ rain.ino
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include "HX711.h" //HX711로드셀 엠프 관련함수 호출
+#define calibration_factor -7050.0 // 로드셀 스케일 값 선언
+#define DOUT1  3 //엠프 데이터 아웃 핀 넘버 선언
+#define CLK1  2  //엠프 클락 핀 넘버 
+#define DOUT2  5 //엠프 데이터 아웃 핀 넘버 선언
+#define CLK2  4  //엠프 클락 핀 넘버   
+#define DISTANCE  3.0  //무게센서 간격(cm)   
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
@@ -11,6 +18,9 @@ U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*
  * BLK -> 주황색 BLA , 노란색 BLK
  */
 
+
+HX711 scale1(DOUT1, CLK1); //엠프 핀 선언 
+HX711 scale2(DOUT2, CLK2); //엠프 핀 선언 
 const int rainSensor = A4;
 
 void setup(void) {
@@ -18,6 +28,10 @@ void setup(void) {
   u8g2.begin();
   u8g2.enableUTF8Print();    // enable UTF8 support for the Arduino print() function
   Serial.begin(9600);
+  scale1.set_scale(calibration_factor);  //스케일 지정 
+  scale1.tare();  //스케일 설정
+  scale2.set_scale(calibration_factor);  //스케일 지정 
+  scale2.tare();  //스케일 설정
   pinMode(rainSensor,INPUT);      // rain sensor
 }
 
@@ -40,10 +54,35 @@ void draw2(void)
   u8g2.print("안전 운행하세요");
 }
 
+void senseWeight() {
+  unsigned long start_time, end_time;
+  int duration = 0;
+  int weightValue2 = 0;
+ 
+  int weightValue1 = scale1.get_units();
+  Serial.print("wegiht1: ");
+  Serial.println(weightValue1);
+  if (weightValue1 >=1){
+    start_time = millis();
+    while(weightValue2<1){
+      int weightValue2 = scale2.get_units();
+      Serial.print("wegiht2: ");
+      Serial.println(weightValue2);
+    }
+    end_time = millis();
+    duration = end_time-start_time/1000;
+    double speed = DISTANCE / duration;
+    Serial.print("speed: ");
+    Serial.println(speed);
+    
+  }
+  delay(100);
+   
+}
 void loop() {
   // 비 감지 센서 값 읽기
   int rainValue = analogRead(rainSensor);
-
+  senseWeight();
   // 비가 내리면 (센서 값이 700 미만일 때)
   if (rainValue < 700) {
     u8g2.firstPage();        // 새로운 페이지 시작
@@ -57,5 +96,5 @@ void loop() {
     } while (u8g2.nextPage()); // 화면 업데이트
   }
 
-  delay(1000);  // 1초 대기 후 다시 반복
+  delay(100);  // 1초 대기 후 다시 반복
 }
